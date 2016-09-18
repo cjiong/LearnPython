e950a714f049db9bfd0c6690027e3470c932bf67
cjiong <416137318@qq.com>
2016-09-18 20:01:54 +0800

    IO.py
    输入输出

---
+++ IO.py
@@ -0,0 +1,25 @@
+# IO.py
+# -*- coding: utf-8 -*-
+
+# output
+print('Hello, Python')
+
+# print()接受多个字符串，用逗号隔开
+# print()会依次打印字符串,遇到逗号会输出一个空格
+print 'Strive','For','Greatness'
+
+print(10086)
+print(10086 + 10010)
+
+print '10086 + 10010 =', 10086 + 10010
+
+
+# input
+
+name = input()
+name
+print(name)
+
+name1 = input('please input your name:')
+print 'Hello', name1
+
