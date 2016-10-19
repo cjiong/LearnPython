3c8099651667702105f154fc6768161e68370935
cjiong <416137318@qq.com>
2016-10-19 22:16:04 +0800

    complete if-else
    条件判断

--- if-else.py
+++ if-else.py
@@ -1,1 +1,23 @@
 # if-else.py
+# 条件判断
+
+# elif 等于 else if
+
+if <条件判断1>:
+    <执行1>
+elif <条件判断2>:
+    <执行2>
+elif <条件判断3>:
+    <执行3>
+else:
+    <执行4>
+
+# if语句是从上往下判断，如果某个判断为True，则把判断对应的执行语句执行后，就忽略掉剩下的elif和else
+
+age = 21
+if age >= 6:
+   print('teenager')
+elif age >= 18:
+   print('adult')
+else:
+   print('kid')
